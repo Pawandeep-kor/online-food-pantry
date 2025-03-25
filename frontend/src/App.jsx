09d8ad9f24ca0998a068b20e868d24494f109503
Pawandeep-kor <148908258+Pawandeep-kor@users.noreply.github.com>
2025-03-25 14:00:08 -0600

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,62 +1,55 @@
-import { useState } from 'react'
-import { Home } from './pages/Home.jsx'
-import { About } from './pages/About.jsx'
-import { ProductDetails } from './pages/ProductDetails.jsx'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import './App.css'
-import { LoginProvider } from './components/TokenProvider.jsx'
-import TestLogin from './pages/TestLogin.jsx'
-import { CartProvider } from './components/CartProvider.jsx'
-
-import {useTranslation} from 'react-i18next';
-import "./i18n.js"
-
-// Add the imports for Login and Signup
-import Login from './pages/login.jsx'
-import Signup from './pages/Signup.jsx'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { LoginProvider } from './components/TokenProvider.jsx';
+import { CartProvider } from './components/CartProvider.jsx';
+import {Home }from './pages/Home.jsx';
+import {About} from './pages/About.jsx';
+import {ProductDetails }from './pages/ProductDetails.jsx';
+import TestLogin from './pages/TestLogin.jsx';
+import Login from './pages/login.jsx';  
+import Signup from './pages/Signup.jsx';
+import './App.css';
+import './i18n.js';
 
 function Header() {
   const { t, i18n } = useTranslation();
 
   const toggleLanguage = () => {
-    const newLang = i18n.language === "en" ? "fr" : "en";
+    const newLang = i18n.language === 'en' ? 'fr' : 'en';
     i18n.changeLanguage(newLang);
-  }
+  };
 
   return (
     <header>
-      <Link to="/" className='link'>{t("home")}</Link>
-      <Link to="/about" className='link'>{t("about")}</Link>
-      <Link to="/testLogin" className='link'>Test Login Page </Link>
-      {/* Add Links for Login and Signup */}
-      <Link to="/login" className='link'>Login</Link>
-      <Link to="/signup" className='link'>Signup</Link>
-      <button onClick={toggleLanguage} className='translate-btn'>{i18n.language === "en" ? "French" : "English"}</button>
+      <Link to="/" className='link'>{t('home')}</Link>
+      <Link to="/about" className='link'>{t('about')}</Link>
+      <Link to="/testLogin" className='link'>Test Login Page</Link>
+      <button onClick={toggleLanguage} className='translate-btn'>
+        {i18n.language === 'en' ? 'French' : 'English'}
+      </button>
     </header>
-  )
+  );
 }
 
 function App() {
   return (
-    <>
-      <LoginProvider>
-        <CartProvider>
-          <BrowserRouter>
-            <Header />
-            {/* Add routes for Login and Signup */}
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="testLogin" element={<TestLogin />} />
-              <Route path="/login" element={<Login />} /> {/* Added Login Route */}
-              <Route path="/signup" element={<Signup />} /> {/* Added Signup Route */}
-              <Route path="/details/:id" element={<ProductDetails />} />
-            </Routes>
-          </BrowserRouter>
-        </CartProvider>
-      </LoginProvider>
-    </>
-  )
+    <LoginProvider>
+      <CartProvider>
+        <BrowserRouter>
+          <Header />
+          {/* Should add a function that includes the search bar and cart icon like our whimsical design */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/testLogin" element={<TestLogin />} />
+            <Route path="/details/:id" element={<ProductDetails />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </BrowserRouter>
+      </CartProvider>
+    </LoginProvider>
+  );
 }
 
-export default App
+export default App;
